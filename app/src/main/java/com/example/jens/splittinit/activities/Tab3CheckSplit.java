7b8c4c1691a503615ed862a7876d2c0d2f6eff05
Paradoxon1994 <timo.gerhard@kabelmail.de>
2018-05-15 12:37:37 +0200

    Check splitter now working

--- app/src/main/java/com/example/jens/splittinit/activities/Tab3CheckSplit.java
+++ app/src/main/java/com/example/jens/splittinit/activities/Tab3CheckSplit.java
@@ -6,10 +6,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.TextView;
 
 import com.example.jens.splittinit.R;
 
 public class Tab3CheckSplit extends Fragment {
+
+    public TextView splittedValue;
+
+    public NumberPicker tipNumberPicker, pplNumberPicker;
+
+    public EditText checkAmount;
+
+    public Button computeButton;
 
 
     @Override
@@ -26,9 +38,40 @@ public class Tab3CheckSplit extends Fragment {
         return rootView;
     }
 
+    @Override
+    public void onStart(){
+        super.onStart();
+
+        computeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double tip = tipNumberPicker.getValue()/100;
+                double numberOfPpl = pplNumberPicker.getValue();
+                double invoiceAmount = Double.parseDouble(checkAmount.getText().toString());
+               double tmp = ((invoiceAmount*tip) + invoiceAmount) / numberOfPpl;
+               String amount = Double.toString(tmp);
+               splittedValue.setText(amount + "€");
+            }
+        });
+    }
+
 
     private void initialize(View v) {
 
+        splittedValue = (TextView) v.findViewById(R.id.splittedValue);
+        tipNumberPicker = (NumberPicker) v.findViewById(R.id.tipNumberPicker);
+        pplNumberPicker = (NumberPicker) v.findViewById(R.id.pplNumberPicker);
+        checkAmount = (EditText) v.findViewById(R.id.checkAmount);
+        computeButton = (Button) v.findViewById(R.id.computeButton);
+
+        tipNumberPicker.setMaxValue(50);
+        tipNumberPicker.setMinValue(1);
+        tipNumberPicker.setValue(20);
+
+        pplNumberPicker.setMaxValue(10);
+        pplNumberPicker.setMinValue(2);
+        pplNumberPicker.setValue(5);
     }
+
 
 }
